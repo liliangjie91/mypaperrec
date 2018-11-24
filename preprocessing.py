3fbdffaa6d02e200bba8ea33e0c7f0ded17c8e58
liliangjie <laiangnaduo91@gmail.com>
2018-11-24 16:22:33 +0800

    new change

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created by lljzhiwang on 2018/11/23
-import os,json,time
+import os,json,time,sys
 import util_common as util
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
+logging.root.setLevel(level=logging.INFO)
+logger.info("running %s" % ' '.join(sys.argv))
+
 datapath='./data/data_raw'
 alllog_b09=datapath+r'/log_b_09.txt'
 alllog_b18=datapath+r'/log_b_18.txt'
@@ -13,9 +20,9 @@ user_typeinter_09 = datapath+r'/userdb_intersec_09.txt'
 user_timeinter_b = datapath+r'/userb_intersec_0918.txt'
 user_timeinter_d = datapath+r'/userd_intersec_0918.txt'
 
-def getchilddic(dicin,keys):
+def get_sub_dic(dicin, keys):
     '''
-    
+    获取子字典，根据keys中的key值
     :param dicin: 
     :type dicin: dict
     :param keys: 
@@ -23,53 +30,30 @@ def getchilddic(dicin,keys):
     :return: 
     :rtype: dict
     '''
+    logger.info("getting sub dicts from input keys...")
     res = {}
-    cnt = 0
-    print("getting child dic...")
     for u in keys:
-        cnt += 1
-        if cnt % (len(keys)/10) == 0:
-            print(cnt)
         res[u] = dicin[u]
     return res
 
-def get_intersec_log_child():
-
-    ul=util.load2list(datapath+r'/userdb_intersec_09.txt')
-    ulogb=util.load2dic(datapath+r'/log_b_09.txt',interval=500000)
-    interlog = getchilddic(ulogb,ul)
-    util.savejson(datapath+r'/ufn_b_typeinter_09.json',interlog)
-
-    del ulogb
-    del interlog
-
-    ulogd = util.loadjson(datapath+r'/log_d_09.json')
-    interlog = getchilddic(ulogd, ul)
-    util.savejson(datapath + r'/ufn_d_typeinter_09.json', interlog)
-
-def getdifference(logb,logd):
-    print("getting diff of two dicts...")
+def get_dic_diff(logb, logd):
+    logger.info("getting two dicts` difference...")
     logdb={}
-    cnt=0
-    alllen=len(logb.keys())
     for i in logb.keys():
-        cnt+=1
-        if cnt%(alllen/10)==0:
-            print cnt
         diff = list(set(logb[i]).difference(set(logd[i])))
         if diff:
             logdb[i]=diff
-    print("length of diff file %d " %len(logdb))
+    logger.info("length of diff file %d " %len(logdb))
     #util.savejson(datapath+'/ufn_typeinter_09_bddiff.json',logdb)
     return logdb
 
 def get_intersec_log(user_interseclist, alllog_b, alllog_d,prefix):
-    print("loading data...")
     uinterl=util.load2list(user_interseclist)
-    blog=util.load2dic(alllog_b, '\t', interval=5000000)
+    blog=util.load2dic(alllog_b, '\t')
     dlog=util.loadjson(alllog_d)
     userb=blog.keys()
     userd=dlog.keys()
+    logger.info("caculating two logs` intersection user...")
     uintertype18=list(set(userb).intersection(set(userd)))
     if len(uintertype18)==len(uinterl):
         print("file %s is correct!" % user_interseclist)
@@ -77,17 +61,18 @@ def get_intersec_log(user_interseclist, alllog_b, alllog_d,prefix):
     else:
         print("file %s is wrong!!!" % user_interseclist)
         return
-    interseced_d = getchilddic(dlog,uinterl)
+    interseced_d = get_sub_dic(dlog, uinterl)
+    interseced_b = get_sub_dic(blog, uinterl)
     del dlog
-    interseced_b = getchilddic(blog,uinterl)
     del blog
-    interseced_dbdiff = getdifference(interseced_b,interseced_d)
-    util.savejson("%s/%s_d.json" %(datapath,prefix),interseced_d)
-    util.savejson("%s/%s_b.json" %(datapath,prefix), interseced_b)
-    util.savejson("%s/%s_dbdiff.json" %(datapath,prefix), interseced_dbdiff)
+    interseced_dbdiff = get_dic_diff(interseced_b, interseced_d)
+    logger.info("saving ress...")
+    util.savejson("%s/%s_d.json",interseced_d)
+    util.savejson("%s/%s_b.json", interseced_b)
+    util.savejson("%s/%s_dbdiff.json", interseced_dbdiff)
+    logger.info("done!")
 
 
 if __name__ == '__main__':
     # get_intersec_childlog()
-    #a=util.loadjson(alllog_d18)
     get_intersec_log(user_typeinter_18,alllog_b18,alllog_d18,"ulog_typeinter18")
--- util_common.py
+++ util_common.py
@@ -5,6 +5,7 @@
 import codecs
 import os,sys,json
 import logging
+import progressbar
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s')
@@ -51,6 +52,7 @@ def list2txt(l,path):
     :param path: 
     :return: 
     '''
+    logger.info("saving list2file : %s" % path)
     if os.path.exists(path):
         logger.info("file %s exists del it.." %path)
         os.remove(path)
@@ -58,7 +60,7 @@ def list2txt(l,path):
         for i in l:
             f.write(i.encode('utf-8', 'ignore')+'\n')
 
-def load2list(path,to1column=False,separator=None,get1column=-1,interval=100000):
+def load2list(path,to1column=False,separator=None,get1column=-1):
     '''
     读取文本返回成list，默认一行一元素，也可按分隔符分割后每部分为一元素，也可单独获取分割后的某列元素为一元素
     :param path: 
@@ -72,14 +74,11 @@ def load2list(path,to1column=False,separator=None,get1column=-1,interval=100000)
     :return: 
     :rtype: list
     '''
+    logger.info("loading file2list : %s" %path)
     res=[]
-    cnt=0
     if os.path.isfile(path):
         with codecs.open(path,'rU',encoding=bianma,errors='replace') as f:
             for l in f:
-                if cnt%interval==0:
-                    print("processing line : %06d" %cnt)
-                cnt+=1
                 if l.strip():
                     if to1column :
                         res.extend(l.split(separator))
@@ -90,7 +89,7 @@ def load2list(path,to1column=False,separator=None,get1column=-1,interval=100000)
     return res
 
 def savejson(path,j):
-
+    logger.info("saving file2json : %s" % path)
     with codecs.open(path,"w",encoding=bianma) as f:
         json.dump(j,f)
 
@@ -102,13 +101,13 @@ def loadjson(path):
     :return: 
     :rtype:dict 
     '''
-    res={}
+    logger.info("loading jsonfile : %s" % path)
     with codecs.open(path, "rU") as f:
         res=json.load(f)
     return res
 
 
-def load2dic(path, separator=None,interval=100000):
+def load2dic(path, separator=None):
     '''
     把文件加载成字典。其中，文件每行第一个元素是key，后续元素组成列表做value
     :param path: 
@@ -116,20 +115,18 @@ def load2dic(path, separator=None,interval=100000):
     :return: 
     :rtype: dict
     '''
+    logger.info("loading file2dic : %s" % path)
     res = {}
-    cnt=0
     if os.path.isfile(path):
         with codecs.open(path, 'rU', encoding=bianma, errors='replace') as f:
             for l in f:
-                if cnt%interval==0:
-                    print("processing line : %06d" %cnt)
-                cnt+=1
                 ll = l.strip().split(separator)
                 if len(ll)>1:
                     res[ll[0]]=ll[1:]
+    print("all lines : %d" %len(res))
     return res
 
-def load2dic_wc(path,separator=None,interval=100000):
+def load2dic_wc(path,separator=None):
     '''
     把文件加载成字典，文件每行每个元素是key，词频是value
     :param path: 
@@ -137,17 +134,15 @@ def load2dic_wc(path,separator=None,interval=100000):
     :return: 
     :rtype: dict
     '''
+    logger.info("loading file2dic_wc : %s" % path)
     res={}
-    cnt=0
     if os.path.isfile(path):
         with codecs.open(path, 'rU', encoding=bianma, errors='replace') as f:
             for l in f:
-                if cnt%interval==0:
-                    print("processing line : %06d" %cnt)
-                cnt+=1
                 ll=l.strip().split(separator)
                 for i in ll:
                     res[i]=res.get(i,0)+1
+    print("all lines : %d" % len(res))
     return res
 
 
