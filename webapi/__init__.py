c4b14736f04d7ba02d5d3a95a6d67db30c37cc3f
liliangjie <laiangnaduo91@gmail.com>
2018-12-12 15:35:39 +0800

    rm webapi

--- webapi/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by xw on 2016/5/31 
-
-import sys
-
-sys.path.append("../")
--- webapi/resource/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Created by xw on 2016/6/22
-
-from flask_restful import reqparse, Resource, marshal_with, abort, fields
-from abstract import Abstract
-
-mod = Abstract()
